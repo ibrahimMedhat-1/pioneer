6a1cf37954e53997e02c56b62dae63151f947522
ibrahimmedhat <doha.kroz0@gmail.com>
2023-08-24 20:11:13 +0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pioneer/view/login/login.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: LoginScreen(),
