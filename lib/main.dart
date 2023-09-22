2b4809a12717f501991ac56655a132c6b8bb93ad
ibrahimmedhat <doha.kroz0@gmail.com>
2023-09-22 16:59:24 +0300

    refactor DB windows

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,19 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // await FirebaseDartFlutter.setup();
+  // Firestore.initialize("dentist-system-cbc47");
+  // await Firebase.initializeApp(
+  //   options: const FirebaseOptions(
+  //       apiKey: "AIzaSyAetq0h2Pih_E0UqHO53zb9xjT5pPAOfZk",
+  //       authDomain: "dentist-system-cbc47.firebaseapp.com",
+  //       projectId: "dentist-system-cbc47",
+  //       storageBucket: "dentist-system-cbc47.appspot.com",
+  //       messagingSenderId: "520181459755",
+  //       appId: "1:520181459755:web:70cf3e69d779cc4d7df36e",
+  //       measurementId: "G-B0MMKBHYV9"),
+  // );
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
